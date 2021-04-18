f15d7a9df483160d7e815d128670eaf3c46963c1
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2021-04-18 16:22:51 +0530

    Update config.js

--- src/authentication/config.js
+++ src/authentication/config.js
@@ -4,13 +4,13 @@ import 'firebase/firebase-auth'
 import 'firebase/storage'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyAsReQvLqNvyHHFdkNMgswzlkHsqn4b-eg",
-    authDomain: "alarm-a709f.firebaseapp.com",
-    databaseURL: "https://alarm-a709f.firebaseio.com",
-    projectId: "alarm-a709f",
-    storageBucket: "alarm-a709f.appspot.com",
-    messagingSenderId: "123772005102",
-    appId: "1:123772005102:web:4a4c7b274bc0e58edb61ff"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
   // Initialize Firebase
 const fb = firebase.initializeApp(firebaseConfig);
