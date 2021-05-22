279c5616ed0dfa416cda1c92dc4b01e97a3ecb85
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2021-05-22 19:57:30 +0530

    Add files via upload

--- src/screens/recruiterhome.js
+++ src/screens/recruiterhome.js
@@ -1,21 +1,21 @@
 import React, { useState } from 'react'
 import { View, Text, Alert, ScrollView } from 'react-native'
 import home from './camera'
-import { Checkbox, TextInput,Button } from 'react-native-paper'
+import { Checkbox, TextInput, Button } from 'react-native-paper'
 import { styles } from '../styles/global'
 import axios from 'axios'
 
-export default function recuriterhome({navigation}) {
+export default function recuriterhome({ navigation }) {
 
     const [checked, setChecked] = useState(false);
     const [projectChecked, setProjectChecked] = useState(true)
     const [numform, setNumform] = useState(['form1'])
     const [forms, setForms] = useState({ form1: { id: 1, question: 'Enter the question', opt1: 'enter the option1', opt2: 'enter the option2', opt3: 'enter the option3', opt4: 'enter the option4' } })
     const [arrs, setArrs] = useState([])
-    const [companyName,setCompanyName] = useState('')
+    const [companyName, setCompanyName] = useState('')
     const [project, setProject] = useState('')
     const [info, setInfo] = useState(null)
-    const [description, setDescription] =  useState('') 
+    const [description, setDescription] = useState('')
     const [position, setPosition] = useState('')
     const [optional, setOptional] = useState('')
     const [location, setLocation] = useState('')
@@ -45,30 +45,50 @@ export default function recuriterhome({navigation}) {
     }
 
     function submit() {
-        if(description!= '' && companyName!='' && position!='' && location!='' && salary !='' )
-        {
-            axios.post('https://particle-ae921-default-rtdb.firebaseio.com/media.json',{
-                project:project,    
-                companyname:companyName,
-                description:description,
-                position:position,
-                optional:optional,
-                location:location,
-                salary:salary,
-                form:forms,
-            }).then(res=>{
+        if (description != '' && companyName != '' && position != '' && location != '' && salary != '') {
+            axios.post('https://particle-ae921-default-rtdb.firebaseio.com/media.json', {
+                project: project,
+                companyname: companyName,
+                description: description,
+                position: position,
+                optional: optional,
+                location: location,
+                salary: salary,
+                form: forms,
+            }).then(res => {
                 console.log(res)
                 setInfo('submitted')
                 navigation.push('Successfullyposted')
             })
-            .catch(err=>console.log(err))
-        }else{
+                .catch(err => console.log(err))
+        } else {
             setInfo('please enter all the inputs')
-            setTimeout(  function()
-            {
+            setTimeout(function () {
                 setInfo(null)
-            }, 3000 )
-        }       
+            }, 3000)
+        }
+    }
+
+    function submit1()
+    {
+        //description != '' && companyName != '' && position != '' && location != '' && salary != ''
+        if (true) {
+            navigation.navigate('Recruitercamera',{
+                project: project,
+                companyname: companyName,
+                description: description,
+                position: position,
+                optional: optional,
+                location: location,
+                salary: salary,
+                form: forms,
+            })
+        } else {
+            setInfo('please enter all the inputs')
+            setTimeout(function () {
+                setInfo(null)
+            }, 3000)
+        }
     }
 
     function allforms(datas) {
@@ -83,51 +103,51 @@ export default function recuriterhome({navigation}) {
 
 
         return (
-            <View style={{flex:1,flexDirection:'column'}} >
+            <View style={{ flex: 1, flexDirection: 'column' }} >
                 { checked && <ScrollView>
                     {
-                        arr.map((form,index) => (
-                            <View style={{flex:1}} key={form.id}>
-                                <Button mode="outlined" >  {'Question ' +  parseInt(index+1)  } </Button>
-                                <TextInput  style={{  margin: 5, backgroundColor:'#ddddd', padding:5 }}  onChangeText={(value) => {
-                                    let uform={...forms}
-                                    let formkey=form.key
-                                    uform[form.key]={...forms[formkey],question:value}
+                        arr.map((form, index) => (
+                            <View style={{ flex: 1 }} key={form.id}>
+                                <Button mode="outlined" >  {'Question ' + parseInt(index + 1)} </Button>
+                                <TextInput style={{ margin: 5, backgroundColor: '#ddddd', padding: 5 }} onChangeText={(value) => {
+                                    let uform = { ...forms }
+                                    let formkey = form.key
+                                    uform[form.key] = { ...forms[formkey], question: value }
                                     setForms(uform)
                                 }} label={'Enter the question'} />
-                                <TextInput  style={{  margin: 5, backgroundColor:'#ddddd' }}   onChangeText={(value) => {
-                                    let uform={...forms}
-                                    let formkey=form.key
-                                    uform[form.key]={...forms[formkey],opt1:value}
+                                <TextInput style={{ margin: 5, backgroundColor: '#ddddd' }} onChangeText={(value) => {
+                                    let uform = { ...forms }
+                                    let formkey = form.key
+                                    uform[form.key] = { ...forms[formkey], opt1: value }
                                     setForms(uform)
                                 }} label={'choice 1'} />
-                                <TextInput style={{  margin: 5, backgroundColor:'#ddddd' }}  onChangeText={(value) => {
-                                    let uform={...forms}
-                                    let formkey=form.key
-                                    uform[form.key]={...forms[formkey],opt2:value}
+                                <TextInput style={{ margin: 5, backgroundColor: '#ddddd' }} onChangeText={(value) => {
+                                    let uform = { ...forms }
+                                    let formkey = form.key
+                                    uform[form.key] = { ...forms[formkey], opt2: value }
                                     setForms(uform)
                                 }} label={'choice 2'} />
-                                <TextInput style={{  margin: 5, backgroundColor:'#ddddd' }}  onChangeText={(value) => {
-                                    let uform={...forms}
-                                    let formkey=form.key
-                                    uform[form.key]={...forms[formkey],opt3:value}
+                                <TextInput style={{ margin: 5, backgroundColor: '#ddddd' }} onChangeText={(value) => {
+                                    let uform = { ...forms }
+                                    let formkey = form.key
+                                    uform[form.key] = { ...forms[formkey], opt3: value }
                                     setForms(uform)
                                 }} label={'choice 3'} />
-                                <TextInput style={{  margin: 5, backgroundColor:'#ddddd' }} onChangeText={(value) => {
-                                    let uform={...forms}
-                                    let formkey=form.key
-                                    uform[form.key]={...forms[formkey],opt4:value}
+                                <TextInput style={{ margin: 5, backgroundColor: '#ddddd' }} onChangeText={(value) => {
+                                    let uform = { ...forms }
+                                    let formkey = form.key
+                                    uform[form.key] = { ...forms[formkey], opt4: value }
                                     setForms(uform)
                                 }} label={'choice 4'} />
 
-                                <View style={{flex:1,flexDirection:'row'}} >  
-                                <Button  onPress={() => deleting(arr, form.id)} style={{ margin:10 ,flex:1, justifyContent:'space-around' }}  mode="contained" > Delete </Button>
-                                <Button  onPress={add} style={{ margin:10,flex:1, justifyContent:'space-between' }}  mode="contained"   >  Add </Button> 
+                                <View style={{ flex: 1, flexDirection: 'row' }} >
+                                    <Button onPress={() => deleting(arr, form.id)} style={{ margin: 10, flex: 1, justifyContent: 'space-around' }} mode="contained" > Delete </Button>
+                                    <Button onPress={add} style={{ margin: 10, flex: 1, justifyContent: 'space-between' }} mode="contained"   >  Add </Button>
                                 </View>
                             </View>
                         ))
                     }
-                   
+
                 </ScrollView>
                 }
             </View>
@@ -137,15 +157,15 @@ export default function recuriterhome({navigation}) {
     return (
         <View style={{ height: 100, flex: 1 }}  >
             <View>
-            { info && <Text style={{justifyContent:'center',textAlign:'center', backgroundColor:'grey' ,fontWeight:'bold'}} > {info}  </Text>}
+                {info && <Text style={{ justifyContent: 'center', textAlign: 'center', backgroundColor: 'grey', fontWeight: 'bold' }} > {info}  </Text>}
             </View>
             <View style={{ marginLeft: 20, marginRight: 20, margin: 10 }}>
                 <ScrollView>
-                    <TextInput style={{  margin: 10, backgroundColor:'#ddddd' }} label='Company Name' onChangeText={(val)=> setCompanyName(val)} />
-                    <TextInput style={{  margin: 10, backgroundColor:'#ddddd' }} label='Description' multiline={true} onChangeText={( (val) => setDescription(val)  )} />
-                    <TextInput style={{  margin: 10, backgroundColor:'#ddddd' }} label='Positions' multiline={true} onChangeText={( (val) => setPosition(val)  )} />
+                    <TextInput style={{ margin: 10, backgroundColor: '#ddddd' }} label='Company Name' onChangeText={(val) => setCompanyName(val)} />
+                    <TextInput style={{ margin: 10, backgroundColor: '#ddddd' }} label='Description' multiline={true} onChangeText={((val) => setDescription(val))} />
+                    <TextInput style={{ margin: 10, backgroundColor: '#ddddd' }} label='Positions' multiline={true} onChangeText={((val) => setPosition(val))} />
 
-                    <View style={{ flexDirection: 'row', justifyContent: 'center',  marginTop: 5 }} >
+                    <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 5 }} >
                         {/* <Checkbox
                             onPress={() => (setProjectChecked(!projectChecked))}
                             status={projectChecked ? 'checked' : 'unchecked'}
@@ -154,19 +174,18 @@ export default function recuriterhome({navigation}) {
                         {/* <Text style={{ fontWeight: 'bold', textAlign: 'center', fontSize: 20, margin: 5 }}  >  Project Section </Text> */}
                     </View>
 
-                    <TextInput  style={{  margin: 10, backgroundColor:'#ddddd' }} label="Location Field" onChangeText={(val)=> setLocation(val)} />
+                    <TextInput style={{ margin: 10, backgroundColor: '#ddddd' }} label="Location Field" onChangeText={(val) => setLocation(val)} />
 
-                    <TextInput  style={{  margin: 10, backgroundColor:'#ddddd' }} label="Salary Field" onChangeText={(val)=> setSalary(val)} />
+                    <TextInput style={{ margin: 10, backgroundColor: '#ddddd' }} label="Salary Field" onChangeText={(val) => setSalary(val)} />
 
-                    {projectChecked && <TextInput  style={{  margin: 10, backgroundColor:'#ddddd' }} label="Project Details" onChangeText={(val)=> setProject(val)} />}
+                    {projectChecked && <TextInput style={{ margin: 10, backgroundColor: '#ddddd' }} label="Project Details" onChangeText={(val) => setProject(val)} />}
 
-                    <TextInput  style={{  margin: 10, backgroundColor:'#ddddd' }} label="Optional Field" onChangeText={(val)=> setOptional(val)} />
-                    
+                    <TextInput style={{ margin: 10, backgroundColor: '#ddddd' }} label="Optional Field" onChangeText={(val) => setOptional(val)} />
+
 
                     <View style={{ justifyContent: 'center', flexDirection: 'row', marginBottom: 5, marginTop: 10 }}>
 
                         <Checkbox
-                            
                             status={checked ? 'checked' : 'unchecked'}
                             onPress={() => setChecked(!checked)}
                             color='#8A2BE2'
@@ -175,15 +194,14 @@ export default function recuriterhome({navigation}) {
                         <Text style={{ fontWeight: 'bold', textAlign: 'center', fontSize: 20, margin: 5 }} >  Quiz Section </Text>
                     </View>
                     {
-                       allforms(forms)
+                        allforms(forms)
                     }
-                    
-                    <Button onPress={submit} style={{margin:5, }}  mode='contained' >  Post Job  </Button> 
-                    
-                    
+
+                    <Button onPress={submit1} style={{ margin: 5, }} mode='contained' >  Create Short Video  </Button>
+
+
                 </ScrollView>
             </View>
         </View>
     )
 }
-
