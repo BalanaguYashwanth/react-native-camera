dd8f0bd74f25b570e33368dbed95512f42905cf1
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2021-05-22 16:40:30 +0530

    Add files via upload

--- src/styles/global.js
+++ src/styles/global.js
@@ -8,6 +8,76 @@ export const styles = StyleSheet.create({
         padding:20,
     },
 
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+
+    borderbox:{
+        borderWidth: 1,
+        borderColor: "black",
+        width:350,
+        height:10,
+        justifyContent:"center",
+        alignItems:'center',
+        margin:10,
+        marginLeft:'auto',
+        marginRight:'auto',
+    },
+
+
+    input:{
+        borderBottomColor:'grey',
+        borderBottomWidth: 1,
+        marginBottom: 100,
+        padding:10,
+    },
+
+    innerborderbox:{
+        borderWidth:1,
+        borderColor:'black',
+        width:90,
+        height:10,
+        justifyContent:"center",
+        alignItems:'center',
+        marginRight:'auto',
+        backgroundColor:'blue'
+    },
+
+    innerborderbox1:{
+        borderWidth:1,
+        borderColor:'black',
+        width:180,
+        height:10,
+        justifyContent:"center",
+        alignItems:'center',
+        marginRight:'auto',
+        backgroundColor:'blue'
+    },
+
+    innerborderbox2:{
+        borderWidth:1,
+        borderColor:'black',
+        width:270,
+        height:10,
+        justifyContent:"center",
+        alignItems:'center',
+        marginRight:'auto',
+        backgroundColor:'blue'
+    },
+
+    innerborderbox3:{
+        borderWidth:1,
+        borderColor:'black',
+        width:350,
+        height:10,
+        justifyContent:"center",
+        alignItems:'center',
+        marginRight:'auto',
+        backgroundColor:'blue'
+    },
+
     button:{
         backgroundColor:'#DDDDDD',
         padding:10,
@@ -15,6 +85,28 @@ export const styles = StyleSheet.create({
         borderRadius:10,
         margin:10
     },
+
+    logo: {
+        width: 100,
+        height: 100,
+        flex:1,
+        flexDirection:'row'
+    },
+
+    video: {
+        flex:1,
+        alignSelf: 'center',
+        width: 350,
+        height: 600,
+      },
+
+    imagecenter:{
+        width:'100%',
+        flex:1,
+        height:400,
+        alignItems:'center',
+        justifyContent:'center'
+    },  
 
     loginButton:{
         marginTop:5,
